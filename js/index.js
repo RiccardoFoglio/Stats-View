9a8230f1ac9bc8547e4b30398b47ee5364ec0f70
Riccardo Foglio <riccardo.fogliopara@gmail.com>
2024-02-10 19:51:46 +0100

    Roster showing up

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
-let Game = null;
 filegame = "SEA @ GIT - 1705278755.game";
 
 fetch("../data/" + filegame)
   .then(response => response.json())
   .then(data => {
     // Do something with the JSON data
-    console.log(data);
-    Game = data
+    
+    buildRoster(data);
+
+
+
   })
   .catch(error => console.error('Error fetching JSON:', error));
 
@@ -46,3 +48,29 @@ $(document).ready(function() {
         }
     });
 });
+
+
+// ROSTER
+function buildRoster(game){
+  //console.log(game.HomeTeam.Name);
+  
+$(document).ready(function() {
+    $("#Home-Roster").html(game.HomeTeam.Name);
+    $("#Away-Roster").html(game.VisitingTeam.Name);
+
+    let homeRosterTable = $("#Home-Roster-table-body");
+    let awayRosterTable = $("#Away-Roster-table-body");
+
+    buildTeamRoster(game.HomeTeam.Players, homeRosterTable);
+    buildTeamRoster(game.VisitingTeam.Players, awayRosterTable);
+  })
+}
+
+function buildTeamRoster(players, rosterTable){
+  players.forEach(player => {
+    let row = $("<tr>");
+    $("<td>").addClass("text-center").text(player.number).appendTo(row);
+    $("<td>").text(player.Name + " " + player.Surname).appendTo(row);
+    rosterTable.append(row);
+  });
+}
